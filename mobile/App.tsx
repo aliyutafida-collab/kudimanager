f0188470925d48b5c2e86047b4a6b91ccfba1657
aliyutafida-collab <aliyutafida@googlemail.com>
2025-11-19 07:07:48 +0000

    Remove old duplicate App.tsx files to fix Vercel build conflict

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { AuthProvider } from './src/contexts/AuthContext';
-import AppNavigator from './src/navigation/AppNavigator';
-import './src/config/i18n';
 
-export default function App() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <AuthProvider>
-        <AppNavigator />
-        <StatusBar style="auto" />
-      </AuthProvider>
-    </GestureHandlerRootView>
-  );
-}
